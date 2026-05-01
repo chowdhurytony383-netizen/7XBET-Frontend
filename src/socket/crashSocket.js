b997c909d2252cdbcc7b1d6291fb47e7be03b20b
chowdhurytony383-netizen <chowdhurytony383@gmail.com>
2026-05-01 09:35:17 +0600

    Add files via upload

--- src/socket/crashSocket.js
+++ src/socket/crashSocket.js
@@ -1,1 +1,27 @@
+import { io } from 'socket.io-client';
+import { API_ORIGIN } from '../api/client.js';
 
+let socket;
+
+export function getCrashSocket() {
+  if (!socket) {
+    socket = io(API_ORIGIN, {
+      path: '/socket.io',
+      withCredentials: true,
+      transports: ['websocket', 'polling'],
+      autoConnect: false,
+      reconnection: true,
+      reconnectionAttempts: Infinity,
+      reconnectionDelay: 500,
+      reconnectionDelayMax: 3000,
+    });
+  }
+  return socket;
+}
+
+export function closeCrashSocket() {
+  if (socket) {
+    socket.disconnect();
+    socket = null;
+  }
+}
