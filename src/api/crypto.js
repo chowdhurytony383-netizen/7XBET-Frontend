70500e4855af2ddb661c7929732c5e71062d842c
chowdhurytony383-netizen <chowdhurytony383@gmail.com>
2026-05-04 23:51:01 +0600

    Add files via upload

--- src/api/crypto.js
+++ src/api/crypto.js
@@ -4,4 +4,7 @@ export const CryptoAPI = {
   addresses: () => api.get('/crypto/addresses'),
   refreshAddresses: () => api.post('/crypto/addresses/refresh'),
   deposits: () => api.get('/crypto/deposits'),
+  withdrawOptions: () => api.get('/crypto/withdraw-options'),
+  withdrawals: () => api.get('/crypto/withdrawals'),
+  createWithdrawal: (payload) => api.post('/crypto/withdrawals', payload),
 };
