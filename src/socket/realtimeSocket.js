6f8ae920ab622bb2e133df2a4b52464544cf36a3
chowdhurytony383-netizen <chowdhurytony383@gmail.com>
2026-05-19 02:04:49 +0600

    Add files via upload

--- src/socket/realtimeSocket.js
+++ src/socket/realtimeSocket.js
@@ -2,15 +2,8 @@ import { io } from 'socket.io-client';
 import { API_ORIGIN, getStoredAccessToken } from '../api/client.js';
 
 let socket;
-let lastAuthToken = '';
-
-function currentToken() {
-  return getStoredAccessToken() || '';
-}
 
 export function getRealtimeSocket() {
-  const token = currentToken();
-
   if (!socket) {
     socket = io(API_ORIGIN, {
       path: '/socket.io',
@@ -21,41 +14,16 @@ export function getRealtimeSocket() {
       reconnectionAttempts: Infinity,
       reconnectionDelay: 500,
       reconnectionDelayMax: 3000,
-      auth: { token },
     });
-    lastAuthToken = token;
   }
 
-  socket.auth = { token };
+  socket.auth = { token: getStoredAccessToken() };
   return socket;
-}
-
-export function connectRealtimeSocket() {
-  const token = currentToken();
-  const instance = getRealtimeSocket();
-
-  // Socket auth is read on connection handshake. If login token changed after the
-  // socket was created, reconnect so support/notification rooms join correctly.
-  if (instance.connected && token !== lastAuthToken) {
-    instance.disconnect();
-  }
-
-  instance.auth = { token };
-  lastAuthToken = token;
-
-  if (!instance.connected) {
-    instance.connect();
-  } else {
-    instance.emit('realtime:auth');
-  }
-
-  return instance;
 }
 
 export function closeRealtimeSocket() {
   if (socket) {
     socket.disconnect();
     socket = null;
-    lastAuthToken = '';
   }
 }
