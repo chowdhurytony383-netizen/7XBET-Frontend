01c8c7c29e8b1eaa752d558dae87008d5447f8cd
chowdhurytony383-netizen <chowdhurytony383@gmail.com>
2026-05-24 02:13:45 +0600

    Add files via upload

--- src/api/sports.js
+++ src/api/sports.js
@@ -2,7 +2,7 @@ import api from './client.js';
 
 export const SportsAPI = {
   categories: () => api.get('/sports/categories'),
-  liveMatches: () => api.get('/sports/live-matches'),
+  liveMatches: (params = {}) => api.get('/sports/live-matches', { params }),
   matchOfTheDay: () => api.get('/sports/match-of-the-day'),
   eventDetails: (eventId) => api.get(`/sports/events/${eventId}`),
   syncStatus: () => api.get('/sports/sync-status'),
